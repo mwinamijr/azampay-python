6d76e96eea2e0d797461041f39d0355bbc5a580c
mwinamijr <mwinamijr@gmail.com>
2023-09-30 18:57:12 +0300

    created generate token request

--- payments/urls.py
+++ payments/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from .views import generate_token
+
 urlpatterns = [
-    
+    path('generate-token', generate_token),
 ]
--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+import requests
+
+from decouple import config
+
+@api_view(['GET'])
+def generate_token(request):
+    baseUrl = config('BASE_URL')
+    appName = config('APP_NAME')
+    clientId = config('CLIENT_ID')
+    clientSecret = config('CLIENT_SECRET')
+    response = requests.post(baseUrl, {appName: appName, clientId: clientId, clientSecret: clientSecret})
+    
+    return Response(response)
