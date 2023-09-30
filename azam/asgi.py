ace44d15ecf9125269c854c887633650598556aa
mwinamijr <mwinamijr@gmail.com>
2023-09-30 13:38:14 +0300

    created new django project

---
+++ azam/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for azam project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'azam.settings')
+
+application = get_asgi_application()
